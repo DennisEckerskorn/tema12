0fc1f3e0db94921b0b2d0ee6f91696eef3ba1448
Dennis Eckerskorn <denniseckerskorn@gmail.com>
2024-08-13 22:56:35 +0200

    Added new method, not finished

--- src/main/java/com/denniseckerskorn/Main.java
+++ src/main/java/com/denniseckerskorn/Main.java
@@ -1,9 +1,18 @@
 package com.denniseckerskorn;
 
+import com.denniseckerskorn.ejercicio05.GestionArchivos;
+
 public class Main {
     public static void main(String[] args) {
         // Ejercicio01_02 ejercicio0102 = new Ejercicio01_02();
-        Ejercicio04 ejercicio04 = new Ejercicio04();
+        // Ejercicio04 ejercicio04 = new Ejercicio04();
+        GestionArchivos ga = new GestionArchivos();
+        String ruta = "C:\\Users\\Dennis\\tema12\\ficheros";
 
+        if (ga.crearArchivo(ruta, "tr5.txt")) {
+            System.out.println("El archivo se ha creado correctamente ");
+        } else {
+            System.out.println("No se ha podido crear al archivo");
+        }
     }
 }
--- src/main/java/com/denniseckerskorn/ejercicio05/GestionArchivos.java
+++ src/main/java/com/denniseckerskorn/ejercicio05/GestionArchivos.java
@@ -1,5 +1,55 @@
 package com.denniseckerskorn.ejercicio05;
 
-public class GestionArchivos {
+import java.io.File;
+import java.io.IOException;
 
+public class GestionArchivos {
+    private File file;
+
+    public GestionArchivos() {
+        file = null;
+    }
+
+    /**
+     * Crea un archivo en el directorio especificado.
+     * Si el directorio no existe, se crea automáticamente.
+     * Luego se intenta crear el archivo en ese directorio.
+     * Si el archivo ya existe o hay un problema al crearlo, se maneja la excepción y se devuelve false.
+     *
+     * @param directorio ruta donde se va a crear el archivo
+     * @param archivo    nombre del archivo que se va a crear (incluyendo la extensión)
+     * @return devuelve true si el archivo se ha creado correctamente, de lo contrario devuelve false
+     */
+    public boolean crearArchivo(String directorio, String archivo) {
+        file = new File(directorio);
+        if (!file.exists()) {
+            if (!file.mkdir()) {
+                return false;
+            }
+        }
+
+        if (file.isDirectory()) {
+            File newFile = new File(directorio, archivo);
+            try {
+                if (newFile.createNewFile()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (IOException ioe) {
+                System.out.println("Error al crear al archivo " + ioe.getMessage());
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public void listarDirectorio(String directorio) {
+
+    }
+
+    public void verInfo(String directorio, String archivo) {
+
+    }
 }
